0e89665db2e49f260e83762f32a6aabcb2f9c731
James Brumond <james@jbrumond.me>
2018-06-29 15:43:16 -0700

    bootstraping project

---
+++ webpack.config.js
@@ -0,0 +1,39 @@
+const path = require('path')
+
+module.exports = {
+  target: 'node',
+  entry: 'src/index.ts',
+  devtool: 'source-map',
+  context: __dirname,
+  output: {
+    path: path.resolve(__dirname, 'dist'),
+    filename: 'index.js',
+    pathinfo: true,
+    libraryTarget: 'umd',
+    devtoolModuleFilenameTemplate: 'webpack-terminus-title-control:///[resource-path]',
+  },
+  resolve: {
+    modules: ['.', 'src', 'node_modules'].map(x => path.join(__dirname, x)),
+    extensions: ['.ts', '.js'],
+  },
+  module: {
+    loaders: [
+      {
+        test: /\.ts$/,
+        loader: 'awesome-typescript-loader',
+        query: {
+          configFileName: path.resolve(__dirname, 'tsconfig.json'),
+        }
+      },
+      { test: /\.pug$/, use: ['apply-loader', 'pug-loader'] },
+      { test: /\.scss$/, use: ['to-string-loader', 'css-loader', 'sass-loader'] },
+    ]
+  },
+  externals: [
+    'fs',
+    /^rxjs/,
+    /^@angular/,
+    /^@ng-bootstrap/,
+    /^terminus-/,
+  ]
+}
